ee9a7266987238b5494b66f836fc4a498c54cf84
kchranger <kchranger@gmail.com>
2025-03-06 17:49:32 +0900

    feat: eas 빌드 환경 변수 설정 #13

---
+++ app.config.js
@@ -0,0 +1,60 @@
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+module.exports = {
+  name: "기도함께",
+  slug: "app-client",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  experiments: {
+    typedRoutes: true,
+    tsconfigPaths: true,
+    newArch: true,
+  },
+  scheme: "pray-together-scheme",
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    softwareKeyboardLayoutMode: "pan",
+    package: "com.changhyeonkim.appclient",
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    "expo-font",
+    [
+      "expo-build-properties",
+      {
+        android: {
+          usesCleartextTraffic: true,
+        },
+      },
+    ],
+  ],
+  extra: {
+    router: {
+      origin: false,
+    },
+    eas: {
+      projectId: "52261da3-842e-4d52-bba7-54ecd152b8d7",
+    },
+  },
+  // SDK 52
+  runtimeEnvs: {
+    EXPO_PUBLIC_API_URL: API_URL ? API_URL : "There is no API URL",
+  },
+};
--- babel.config.js
+++ babel.config.js
@@ -3,26 +3,6 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      [
-        "@tamagui/babel-plugin",
-        {
-          components: ["tamagui"],
-          config: "./tamagui.config.ts",
-          logTimings: true,
-          disableExtraction: process.env.NODE_ENV === "development",
-        },
-      ],
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
       // NOTE: this is only necessary if you are using reanimated for animations
       "react-native-reanimated/plugin",
     ],
