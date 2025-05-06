a26c06b4042557063bab4ceb577a58653272c414
kchranger <kchranger@gmail.com>
2025-05-06 14:29:10 +0900

    feat: 알림 설정 UI 변경 #39
    - 곧 바로 OS 알림 설정으로 이동

--- app/(protected)/(tabs)/my-page/index.tsx
+++ app/(protected)/(tabs)/my-page/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Linking, Platform } from "react-native";
 import { useRouter } from "expo-router";
 import { backgroundColor } from "@/common/styles/color";
 
-import Top4Body10 from "@/common/layout/Top4Body10"; // Adjust path as needed
+import Top4Body10 from "@/common/layout/Top4Body10";
 import ProfileSection from "../../my-page/index/_components/ProfileSection";
 import ListSection from "../../my-page/index/_components/ListSection";
 import path from "@/common/constants/path";
@@ -16,14 +16,20 @@ export default function MyPageScreen(props: MyPageScreenProps) {
   const router = useRouter();
   const { mutate: logoutRequest } = useLogoutMutation();
   const { getRefreshToken, logout: setLogoutState } = useAuthStore();
+
   function handleGoToInvitations() {
     console.log("초대 목록 화면으로 이동");
     router.push(path.showInvitations());
   }
 
+  // 알림 설정 함수 수정 - 바로 OS 설정으로 이동
   function handleGoToNotifications() {
-    console.log("알림 설정 화면으로 이동");
-    router.push(path.showNotificationsSettings());
+    console.log("시스템 알림 설정으로 직접 이동");
+    if (Platform.OS === "ios") {
+      Linking.openURL("app-settings:");
+    } else {
+      Linking.openSettings();
+    }
   }
 
   async function handleLogout() {
