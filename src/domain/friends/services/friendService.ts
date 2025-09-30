b00dccebb939e7d8461368477b1520b521aa19f9
kchranger <kchranger@naver.com>
2025-09-30 23:36:46 +0900

    feat: 친구 기능 관련 API 추가 #49
    - 친구 요청
    - 친구 요청 목록 조회
    - 친구 요청 응답
    - 친구 목록 조회
    - 친구 삭제

---
+++ src/domain/friends/services/friendService.ts
@@ -0,0 +1,52 @@
+import apiService from "@/common/apis/apiService";
+import type { MessageResponse } from "@/common/types/messageResponse";
+import { FriendInvitation } from "../types/FriendInvitation";
+import { Friend } from "../types/Friend";
+import { FetchFriendInvitationsResponse } from "../types/response/fetchFriendInvitationsResponse";
+import { FetchFriendsResponse } from "../types/response/fetchFriendsResponse";
+import { UpdateFriendInvitationStatusRequest } from "../types/request/updateFriendInvitationStatusRequest";
+import { FRIEND_INVITATION_STATUS } from "../constants/friendInvitationStatus";
+
+export const friendService = {
+  // 1. 친구 요청 보내기
+  sendFriendInvitation: async (memberId: number): Promise<MessageResponse> => {
+    const response = await apiService.post<MessageResponse>(
+      `/friends/${memberId}/requests`
+    );
+    return response;
+  },
+
+  // 2. 친구 요청 목록 조회
+  fetchFriendInvitations: async (): Promise<FriendInvitation[]> => {
+    const response = await apiService.get<FetchFriendInvitationsResponse>(
+      `/friends/requests`
+    );
+    return response.data.friendInvitations;
+  },
+
+  // 3. 친구 요청 응답 (수락/거절)
+  updateInvitationStatus: async (
+    requestId: number,
+    status: FRIEND_INVITATION_STATUS
+  ): Promise<MessageResponse> => {
+    const response = await apiService.patch<MessageResponse>(
+      `/friends/requests/${requestId}`,
+      { status } as UpdateFriendInvitationStatusRequest
+    );
+    return response;
+  },
+
+  // 4. 친구 목록 조회
+  fetchFriends: async (): Promise<Friend[]> => {
+    const response = await apiService.get<FetchFriendsResponse>(`/friends`);
+    return response.data.friends;
+  },
+
+  // 5. 친구 삭제
+  deleteFriend: async (friendId: number): Promise<MessageResponse> => {
+    const response = await apiService.delete<MessageResponse>(
+      `/friends/${friendId}`
+    );
+    return response;
+  },
+};
