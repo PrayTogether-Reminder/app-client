8d9b48226edb41cb504ae6b2944af57709eff01c
kchranger <kchranger@naver.com>
2025-11-12 19:38:23 +0900

    chore: Android 업데이트 URL 변경
    - 구글 플레이 스토어 공식 URL로 변경

--- src/hooks/useAppUpdateModal.tsx
+++ src/hooks/useAppUpdateModal.tsx
@@ -96,7 +96,7 @@ export const useAppUpdateModal = () => {
   const openStore = () => {
     const storeUrl = Platform.select({
       ios: 'https://apps.apple.com/app/id6738428439',
-      android: 'https://play.google.com/apps/testing/site.praytogether',
+      android: 'https://play.google.com/store/apps/details?id=site.praytogether',
     });
 
     if (storeUrl) {
