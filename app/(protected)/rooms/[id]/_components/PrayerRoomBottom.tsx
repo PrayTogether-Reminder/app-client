10cea2e2029bf28552fb13fc342297d0417473bb
kchranger <kchranger@naver.com>
2025-12-06 22:43:36 +0900

    refactor: 기도 제목 작성 버튼 공통화 적용

--- app/(protected)/rooms/[id]/_components/PrayerRoomBottom.tsx
+++ app/(protected)/rooms/[id]/_components/PrayerRoomBottom.tsx
@@ -1,62 +1,30 @@
 import React, { useState } from "react";
-import { StyleSheet } from "react-native";
-import { Surface, Button, useTheme } from "react-native-paper";
-import { RFValue } from "react-native-responsive-fontsize";
-import { color } from "../../../../../src/common/styles/color";
-import { useRouter } from "expo-router";
-import path from "../../../../../src/common/constants/path";
+import { BottomActionButton } from "@/common/components/button";
 import PrayerTitleCreationDialog from "./dialogs/PrayerTitleCreationDialog";
 
 interface PrayerRoomBottomButtonProps {}
 
 const PrayerRoomBottomButton: React.FC<PrayerRoomBottomButtonProps> = ({}) => {
-  const router = useRouter();
   const [dialogOpen, setDialogOpen] = useState(false);
-  
+
   const handlePress = () => {
     setDialogOpen(true);
   };
 
   return (
     <>
-      <Surface style={styles.bottomButtonContainer}>
-        <Button
-          mode="contained"
-          uppercase={false}
-          style={styles.bottomButton}
-          labelStyle={styles.bottomButtonText}
-          icon="pencil"
-          onPress={handlePress}
-        >
-          기도제목 작성하기
-        </Button>
-      </Surface>
-      
-      <PrayerTitleCreationDialog 
-        open={dialogOpen} 
-        setOpen={setDialogOpen} 
+      <BottomActionButton
+        onPress={handlePress}
+        icon="pencil"
+        text="기도제목 작성하기"
+      />
+
+      <PrayerTitleCreationDialog
+        open={dialogOpen}
+        setOpen={setDialogOpen}
       />
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  bottomButtonContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  bottomButton: {
-    borderRadius: 30,
-    width: "70%",
-    paddingVertical: 5,
-    backgroundColor: color.secondary,
-  },
-  bottomButtonText: {
-    fontSize: RFValue(16),
-    fontWeight: "500",
-    lineHeight: RFValue(22),
-  },
-});
 
 export default PrayerRoomBottomButton;
