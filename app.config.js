d23ffa70d15b3e9eb1ca18b6a219ece367c24ea3
kchranger <kchranger@naver.com>
2026-01-01 15:39:10 +0900

    chore: 배포
     - version 1.0.12
     - android 23
     - ios 1.0.24

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ console.log("EXPO_PUBLIC_API_URL from env:", process.env.EXPO_PUBLIC_API_URL);
 
 const API_URL ="https://praytogether.site/api";
 // const API_URL ="http://172.30.1.24:8080/api";
-// const API_URL ="https://420691fee64f.ngrok-free.app/api";
+// const API_URL =" https://a614e74aba66.ngrok-free.app/api";
 
 // 환경별 설정
 const environment = process.env.EAS_BUILD_PROFILE || 'production';
@@ -33,7 +33,7 @@ const config = {
 const currentConfig = config[environment] || config.production;
 
 const expoVersion = '1.0.12';
-const androidVersion = 21;
+const androidVersion = 23;
 const iosVersion = '1.0.24';
 
 module.exports = {
