172b164a489e2ad23a0821ef20a63a0b8d83c90f
kchranger <kchranger@naver.com>
2026-04-17 23:48:02 +0900

    feat: react query 캐시 설정 제거
    - UX를 위해 캐시 제거

--- src/common/hooks/queries/customQueryClientProvider.tsx
+++ src/common/hooks/queries/customQueryClientProvider.tsx
@@ -4,11 +4,17 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 2,
-      staleTime: 1000 * 60 * 5, // 5m
-      gcTime: 1000 * 60 * 30, // 30m
-      refetchOnWindowFocus: false,
+      // UX 이슈로 캐싱 일시 비활성화
+      // staleTime: 1000 * 60 * 5, // 5m
+      // gcTime: 1000 * 60 * 30, // 30m
+      staleTime: 0,
+      gcTime: 0,
+      // UX 이슈로 캐싱 일시 비활성화 (앱 복귀/재연결 시에도 refetch)
+      // refetchOnWindowFocus: false,
+      // refetchOnReconnect: false,
+      refetchOnWindowFocus: true,
       refetchOnMount: true,
-      refetchOnReconnect: false,
+      refetchOnReconnect: true,
     },
   },
 });
